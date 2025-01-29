45ea7609b7bf1b6c29e4f1e93e428a59d109d231
Pesho Ivanov <ivanov@pesho.info>
2025-01-29 16:23:23 -0500

    ASSERT not terminating

--- src/utils.h
+++ src/utils.h
@@ -10,13 +10,15 @@
 
 #include "types.h"
 
+//            std::terminate(); \
+
 #ifndef NDEBUG
 #   define ASSERT(condition, message) \
     do { \
         if (! (condition)) { \
             std::cerr << "Assertion `" #condition "` failed in " << __FILE__ \
                       << " line " << __LINE__ << ": " << message << std::endl; \
-            std::terminate(); \
+            throw std::runtime_error("Assertion failure"); \
         } \
     } while (false)
 #else
